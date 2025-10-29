3870ee8a3b2b2ef4d98fda3b36adead1b1be9b9b
Ayush Singh <ayushsingh1012p@gmail.com>
2025-10-29 20:35:35 +0530

    clean commit

--- chatbot/main.py
+++ chatbot/main.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 
-# Download required NLTK data silently
 nltk.download('punkt', quiet=True)
 nltk.download('wordnet', quiet=True)
 
