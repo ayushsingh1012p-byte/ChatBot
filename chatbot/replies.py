860361fd6e5199fe7f81a8728cbf33f7ca38579a
Ayush Singh <ayushsingh1012p@gmail.com>
2025-11-05 21:37:38 +0530

    commits

--- chatbot/replies.py
+++
@@ -1,7 +0,0 @@
-from . import main
-
-def bot_reply(query):
-    assistant = main.ChatbotAssistant(main.INTENTS_PATH)
-    assistant.parse_intents()
-    assistant.load_model("chatbot_model.pth", "dimensions.json")
-    return assistant.process_message(query)
